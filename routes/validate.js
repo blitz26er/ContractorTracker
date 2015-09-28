9b92d97f5161464913bb8c37ca7aaa6bc8acc22a
git <blitz26er@gmail.com>
2015-09-28 22:29:28 +0800

    first

--- routes/validate.js
+++ routes/validate.js
@@ -24,7 +24,7 @@ router.use(function(req, res, next) {
         });
     } else {
         // if there is no token
-        return res.status(500).send({success: false, message: 'No token provided.'});
+        return res.status(500).json({success: false, message: 'No token provided.'});
     }
 });
 module.exports = router;
