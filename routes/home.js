0e20202efa7d2f8ef0f3ec655369669e6c9b9662
git <blitz26er@gmail.com>
2015-10-09 23:39:39 +0800

    first

--- routes/home.js
+++ routes/home.js
@@ -64,7 +64,7 @@ router.route('/home/recent')
                         var jobreport_item = jobreport.toObject();
                         for(var k=0, nk=jobs.length; k<nk; k++) {
                             var job = jobs[k];
-                            if(job._id == jobreport.job_id) {
+                            if(job._id.equals(jobreport_item.job_id)) {
                                 delete jobreport_item.name;
                                 delete jobreport_item.description;
                                 delete jobreport_item.job_id;
@@ -72,8 +72,8 @@ router.route('/home/recent')
                                 delete jobreport_item.user_id;
                                 var tasks = jobreport_item.tasks;
                                 var task_list = [];
-                                for(l=0, nl<jobreport_item.tasks.length; l<nl; l++) {
-                                    task = tasks[i];
+                                for(var l=0, nl=jobreport_item.tasks.length; l<nl; l++) {
+                                    task = tasks[l];
                                     if(task.work_period != 0 && task.work_period != null && task.work_period != '') {
                                         task_list.push(task);
                                     }
@@ -99,7 +99,6 @@ router.route('/home/recent')
                     }
                 }
             }
-            console.log(list);
             res.json(list);
         });
     });
